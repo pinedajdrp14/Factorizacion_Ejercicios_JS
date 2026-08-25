3a03b7c0f576aa2d2d1de162ece800de505bd49e
JhonathanGitNeda <pineda.jdrp14@gmail.com>
2026-08-24 20:54:57 -0500

    Update script.js

--- Ejercicio-3/script.js
+++ Ejercicio-3/script.js
@@ -2,4 +2,4 @@ var celsius = prompt("Ingrese la temperatura en °C: ")
 
 var Farenheit = (celsius * 9/5) + 32
 
-alert("La temperatura en Farenheit es igual a: " + Farenheit + "°F" )  
+document.write("La temperatura en Farenheit es igual a: " + Farenheit + "°F" )  
