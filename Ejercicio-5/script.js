def0693b4c4f7703f71ea97d3608efe63bd48c7f
JhonathanGitNeda <pineda.jdrp14@gmail.com>
2026-08-24 20:04:57 -0500

    Add files via upload

---
+++ Ejercicio-5/script.js
@@ -0,0 +1,9 @@
+var a = Number(prompt("Ingrese el primer numero: "))
+var b = Number(prompt("Ingrese el segundo numero: "))
+var auxiliar = a
+
+a = b
+b = auxiliar
+
+alert("El valor de a es " + a);
+alert("El valor de b es " + b);
