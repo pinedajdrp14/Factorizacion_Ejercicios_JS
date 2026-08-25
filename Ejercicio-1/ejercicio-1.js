e3832e52b65b24ac0d6959ad3c33e368969d79c0
JhonathanGitNeda <pineda.jdrp14@gmail.com>
2026-08-24 20:58:28 -0500

    Update ejercicio-1.js

--- Ejercicio-1/ejercicio-1.js
+++ Ejercicio-1/ejercicio-1.js
@@ -2,4 +2,4 @@ Var Altura = Number.prompt("Ingrese el valor de la altura: ")
 Var base = Number.prompt("Ingrese el valor de la base: ")
 Var Area = (base*Altura) /2
 
-alert (“El área de un triángulo es igual a “ + Area);
+document.write(“El área de un triángulo es igual a “ + Area);
