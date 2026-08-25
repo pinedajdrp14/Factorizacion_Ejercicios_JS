84446355dbda61eab9fdd36afe3e6ec99d28d456
JhonathanGitNeda <pineda.jdrp14@gmail.com>
2026-08-24 19:40:21 -0500

    Add files via upload

---
+++ Ejercicio-2/script.js
@@ -0,0 +1,10 @@
+var altura = Number(prompt("Ingrese la altura del rectangulo: "));
+
+var base = Number(prompt("Ingrese la base del rectangulo: "));
+
+let area = base * altura;
+let perimetro = 2 * (base + altura)
+
+alert("El area del rectangulo es igual a " + area);
+
+alert("El perimetro del rectangulo es igual a " + perimetro); 
