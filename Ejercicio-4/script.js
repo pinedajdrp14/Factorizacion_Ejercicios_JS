d86121a8fbe84082cf729ce387d203be2cc6d36c
JhonathanGitNeda <pineda.jdrp14@gmail.com>
2026-08-24 20:00:16 -0500

    Add files via upload

---
+++ Ejercicio-4/script.js
@@ -0,0 +1,9 @@
+var numero1 = Number(prompt("Ingrese el primer numero: "))
+
+var numero2 = Number(prompt("Ingrese el segundo numero: "))
+
+var numero3 = Number(prompt("Ingrese el tercer numero: "))
+
+var promedio = (numero1 + numero2 + numero3) / 3
+
+alert("El promedio es igual a " + promedio);
