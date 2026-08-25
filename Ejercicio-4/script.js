552b649b60af80fa4ecc4f938d50230282b482b2
JhonathanGitNeda <pineda.jdrp14@gmail.com>
2026-08-24 20:53:56 -0500

    Update script.js

--- Ejercicio-4/script.js
+++ Ejercicio-4/script.js
@@ -6,4 +6,4 @@ var numero3 = Number(prompt("Ingrese el tercer numero: "))
 
 var promedio = (numero1 + numero2 + numero3) / 3
 
-alert("El promedio es igual a " + promedio);
+document.write("El promedio es igual a " + promedio);
