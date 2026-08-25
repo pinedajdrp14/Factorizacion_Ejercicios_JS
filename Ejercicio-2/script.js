c9de1e162e4d8a02ae695f14b2586df600531264
JhonathanGitNeda <pineda.jdrp14@gmail.com>
2026-08-24 20:55:58 -0500

    Update script.js

--- Ejercicio-2/script.js
+++ Ejercicio-2/script.js
@@ -5,6 +5,6 @@ var base = Number(prompt("Ingrese la base del rectangulo: "));
 let area = base * altura;
 let perimetro = 2 * (base + altura)
 
-alert("El area del rectangulo es igual a " + area);
+document.write("El area del rectangulo es igual a " + area);
 
-alert("El perimetro del rectangulo es igual a " + perimetro); 
+document.write("El perimetro del rectangulo es igual a " + perimetro); 
